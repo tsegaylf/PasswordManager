1e6754ab210b261434849cb8c3d85b2be6690aac
Lidya Tsegay <lidyatsegay@outlook.com>
2019-08-30 09:10:37 -0400

    initial commit

--- PasswordManager/Program.cs
+++ PasswordManager/Program.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PasswordManager {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            var passwordManager = new Dictionary<string, string>();
+            passwordManager.Add("1maxtrain.com", "Ohio123");
+            passwordManager.Add("2maxtrain.com", "Kentucky123");
+            passwordManager.Add("3maxtrain.com", "Indiana123");
+            passwordManager.Add("4maxtrain.com", "California123");
+            passwordManager.Add("5maxtrain.com", "Alaska123");
+            passwordManager.Add("6maxtrain.com", "Florida123");
+            passwordManager.Add("7maxtrain.com", "Atlanta123");
+            passwordManager.Add("8maxtrain.com", "Illinois123");
+            passwordManager.Add("9maxtrain.com", "DC123");
+            passwordManager.Add("11maxtrain.com", "Maryland123");
+            passwordManager.Add("12maxtrain.com", "Colorado123");
+            passwordManager.Add("13maxtrain.com", "Texas123");
+            
+            var password = passwordManager["1maxtrain.com"];
+            Console.WriteLine($"The password for 1maxtrain.com is {password}");
         }
     }
 }
